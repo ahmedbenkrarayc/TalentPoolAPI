f1eb034c8aba5616007a20d3b7c39bfd02590563
ahmedbenkrarayc <ahmed.benkrara2@gmail.com>
2025-03-24 04:58:17 +0000

    created candidature re^psitory

---
+++ app/repositories/CandidatureRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Candidature;
+use App\Repositories\Interfaces\ICandidatureRepository;
+use Illuminate\Support\Facades\Storage;
+
+class CandidatureRepository implements ICandidatureRepository
+{
+    public function create(array $data)
+    {
+        return Candidature::create($data);
+    }
+
+    public function getByUser(int $userId)
+    {
+        return Candidature::with('candidate', 'annonce')->where('candidate_id', $userId)->get();
+    }
+
+    public function delete(int $id)
+    {
+        $candidature = Candidature::find($id);
+        if (!$candidature) {
+            return false;
+        }
+
+        Storage::delete([$candidature->cv, $candidature->coverletter]);
+
+        return $candidature->delete();
+    }
+
+    public function getAll()
+    {
+        return Candidature::with('candidate', 'annonce')->get();
+    }
+
+    public function getById(int $id)
+    {
+        return Candidature::with('candidate', 'annonce')->find($id);
+    }
+}
